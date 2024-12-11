4bba2e1a6d911e2f8a04c2dc3d039d0ee47df912
BagusKurniawan02 <bagus.kurniawan@student.president.ac.id>
2024-12-12 00:06:51 +0700

    s7

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ def convert_image_to_bytes(img, format_type):
     return img_byte_arr
 
 # Layout Streamlit
-st.image("https://www.president.ac.id/images/favicon/apple-touch-icon.png", width=100)
+st.image("https://th.bing.com/th/id/OIP.5HriVYJ9IdpM-pGiZPSU6wHaEK?rs=1&pid=ImgDetMain", width=100)
 st.title("Image Editor")
 st.write("Upload an image and edit it with rotation, brightness, scaling, and RGB color adjustments.")
 
@@ -52,7 +52,7 @@ if uploaded_file is not None:
         img_rotated = rotate_image(img, rotation_angle)
     else:
         # Automatic rotation
-        rotation_angle = st.selectbox("Select Rotation Angle", [45, 90, 135, 180, 225, 270, 315, 360])
+        rotation_angle = st.selectbox("Select Rotation Angle", [0, 45, 90, 135, 180, 225, 270, 315, 360])
         img_rotated = rotate_image(img, rotation_angle)
 
     st.image(img_rotated, caption="Rotated Image", use_container_width=True)
